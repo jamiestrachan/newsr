cba7c2d677585c30477e1c7f2ef1b572ebc64f41
Jamie Strachan <jamie.strachan@gmail.com>
2015-03-13 00:46:03 -0400

    roughing in list recommendation

--- src/js/newsr.js
+++ src/js/newsr.js
@@ -6,6 +6,11 @@ var newsr = (function () {
 	templates.scanItem = '<h1>{{title}}</h1><h2>{{deck}}</h2><p>{{description}}</p>';
 
 	// private members
+	/* Do necessary setup when switching modes */
+	function setMode(mode) {
+		dom.body.className = "mode_" + mode;
+	}
+
 	/* Get the JSON of a content list from a data source */
 	function getList(source, callback) {
 		var jqxhr = $.getJSON("/json/" + source, function(data) {
@@ -21,7 +26,7 @@ var newsr = (function () {
 					}
 				}
 			}
-			if (listElement.contentlist && listElement.contentlist.contentitems && listElement.contentlist.contentitems.length > 0) {
+			if (listElement && listElement.contentlist && listElement.contentlist.contentitems && listElement.contentlist.contentitems.length > 0) {
 				callback(listElement.contentlist.contentitems);
 			} else {
 				callback(false);
@@ -36,6 +41,7 @@ var newsr = (function () {
 	function showItem() {
 		if (contentList.length > 0) {
 			contentItem = contentList.shift();
+			console.log(contentItem);
 			dom.scanItem.innerHTML = Mustache.render(templates.scanItem, contentItem);
 		} else {
 			recommendList();
@@ -44,24 +50,35 @@ var newsr = (function () {
 
 	/* Suggest new list sources to start the scan process again */
 	function recommendList() {
+		setMode("recommend");
 
+		dom.recommendList.innerHTML = '<li><a href="/scan/news/business/">Business</a></li><li><a href="/scan/news/arts">Arts</a></li><li><a href="/scan/sports">Sports</a></li>';
 	}
 
 	// public variables and members
 	return {
 		init: function () {
 			dom = {};
+			dom.body = document.getElementsByTagName("body")[0];
 			dom.scanItem = document.getElementById("scanitem");
+			dom.scanNext = document.getElementById("scannext");
+			dom.recommendList = document.getElementById("recommendlist");
 			return true;
 		},
 		scan: function (source) {
-			var reqURL = source.split("/scan/")[1];
+			var reqURL;
+
+			setMode("scan");
+
+			reqURL  = source.split("/scan/")[1];
 			if ( reqURL && reqURL.slice(-1) === "/" ) {
 				reqURL = reqURL.substring(0, reqURL.length - 1);
 			}
 			if (!reqURL) {
 				reqURL = "news";
 			}
+
+			dom.scanNext.onclick = showItem;
 
 			getList(reqURL, function (list) {
 				if (list) {
@@ -71,6 +88,9 @@ var newsr = (function () {
 			});
 		},
 		read: function (source) {
+
+			setMode("read");
+
 			return false;
 		}
 	};
